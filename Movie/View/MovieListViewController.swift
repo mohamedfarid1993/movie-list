302561ae940527f1d2275cccfa0f23fa5be2de7d
mfaridghonaim <80623529+mfaridghonaim@users.noreply.github.com>
2024-07-10 16:17:35 +0300

    Add initial view

--- Movie/View/MovieListViewController.swift
+++ Movie/View/MovieListViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MovieListViewController.swift
 //  Movie
 //
 //  Created by Mohamed Farid on 10/07/2024.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MovieListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
